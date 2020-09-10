ca6e8c5165dc99359da60c2be334b158b6791880
CodeVenom99 <1060401004@qq.com>
2020-09-10 19:26:34 +0800

    CFM

--- build/release/CMF.d
+++ build/release/CMF.d
@@ -1,4 +1,4 @@
-build/release/obstruct_num.o: obstruct_num.c \
+build/release/CMF.o: CMF.c \
  /usr/local/webots/include/controller/c/webots/motor.h \
  /usr/local/webots/include/controller/c/webots/types.h \
  /usr/local/webots/include/controller/c/webots/robot.h \
@@ -6,4 +6,5 @@ build/release/obstruct_num.o: obstruct_num.c \
  /usr/local/webots/include/controller/c/webots/distance_sensor.h \
  /usr/local/webots/include/controller/c/webots/compass.h \
  /usr/local/webots/include/controller/c/webots/device.h \
- /usr/local/webots/include/controller/c/webots/pen.h
+ /usr/local/webots/include/controller/c/webots/pen.h \
+ /usr/local/webots/include/controller/c/webots/gps.h
